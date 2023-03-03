a410f350f20a0c05d771e2111f3a8dde725d3f05
Marcus Liu <marcus.liu@stonybrook.edu>
2023-03-02 23:16:01 -0500

    edit

--- HW 2/ analysis_pcap_tcp.py
+++ HW 2/ analysis_pcap_tcp.py
@@ -1,57 +1,27 @@
 import dpkt
 import datetime
 import socket
-from dpkt.compat import compat_ord
-
-
 
 f = open('assignment2.pcap',  'rb')
 pcap = dpkt.pcap.Reader(f)
 
 
-def mac_addr(address):
-    """Convert a MAC address to a readable/printable string
-
-       Args:
-           address (str): a MAC address in hex form (e.g. '\x01\x02\x03\x04\x05\x06')
-       Returns:
-           str: Printable/readable MAC address
-    """
-    return ':'.join('%02x' % compat_ord(b) for b in address)
+def convMac(address):
+    return ":".join("{:02x}".format(b) for b in address)
 
 
-def inet_to_str(inet):
-    """Convert inet object to a string
-
-        Args:
-            inet (inet struct): inet network address
-        Returns:
-            str: Printable/readable IP address
-    """
-    # First try ipv4 and then ipv6
-    try:
-        return socket.inet_ntop(socket.AF_INET, inet)
-    except ValueError:
-        return socket.inet_ntop(socket.AF_INET6, inet)
-
-
-for ts, buf in pcap:
-    print('Timestamp: ', str(datetime.datetime.utcfromtimestamp(ts)))
-    eth = dpkt.ethernet.Ethernet(buf)
-    print('Ethernet Frame: ', mac_addr(eth.src), mac_addr(eth.dst), eth.type)
-
-    if not isinstance(eth.data, dpkt.ip.IP):
-        print('Non IP Packet type not supported %s\n' % eth.data.__class__.__name__)
-        continue
-    ip = eth.data
-
-        
-    do_not_fragment = bool(ip.off & dpkt.ip.IP_DF)
-    more_fragments = bool(ip.off & dpkt.ip.IP_MF)
-    fragment_offset = ip.off & dpkt.ip.IP_OFFMASK
-
-    print('IP: %s -> %s   (len=%d ttl=%d DF=%d MF=%d offset=%d)\n' % \
-        (inet_to_str(ip.src), inet_to_str(ip.dst), ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset))
-
-print(eth)
-
+for ts, bf in pcap:
+    print('Time Stamp: ', str(datetime.datetime.utcfromtimestamp(ts)))
+    eth = dpkt.ethernet.Ethernet(bf)
+    print('Ethernet Frame:')
+    if isinstance(eth.data, dpkt.ip.IP):
+        ip = eth.data
+        if isinstance(ip.data, dpkt.tcp.TCP):
+            tcp = ip.data
+            src_port = tcp.sport
+            dst_port = tcp.dport
+            src_ip = socket.inet_ntoa(ip.src)
+            dst_ip = socket.inet_ntoa(ip.dst)
+            print(f"Source IP: {src_ip}, Source Port: {src_port}")
+            print(f"Destination IP: {dst_ip}, Destination Port: {dst_port}\ns")
+    
