4d263ce28b90114c63c9af1af07c189ab165b13b
Marcus Liu <marcus.liu@stonybrook.edu>
2023-03-03 17:38:35 -0500

    Edit

--- HW 2/ analysis_pcap_tcp.py
+++ HW 2/ analysis_pcap_tcp.py
@@ -2,22 +2,16 @@ import dpkt
 import datetime
 import socket
 
-f = open('assignment2.pcap',  'rb')
+f = open('assignment2.pcap', 'rb')
 pcap = dpkt.pcap.Reader(f)
 
 def convMac(address):
     return ":".join("{:02x}".format(b) for b in address)
 
-def inet_to_str(inet):
-    try:
-        return socket.inet_ntop(socket.AF_INET, inet)
-    except ValueError:
-        return socket.inet_ntop(socket.AF_INET6, inet)
+flows = {}
 
 for ts, bf in pcap:
-    print('Time Stamp: ', str(datetime.datetime.utcfromtimestamp(ts)))
     eth = dpkt.ethernet.Ethernet(bf)
-    print('Ethernet Frame:')
     if isinstance(eth.data, dpkt.ip.IP):
         ip = eth.data
         if isinstance(ip.data, dpkt.tcp.TCP):
@@ -26,34 +20,57 @@ for ts, bf in pcap:
             dstPort = tcp.dport
             srcIp = socket.inet_ntoa(ip.src)
             dstIp = socket.inet_ntoa(ip.dst)
-            print(f"Source IP: {srcIp}, Source Port: {srcPort}")
-            print(f"Destination IP: {dstIp}, Destination Port: {dstPort}")
-            do_not_fragment = bool(ip.off & dpkt.ip.IP_DF)
-            more_fragments = bool(ip.off & dpkt.ip.IP_MF)
-            fragment_offset = ip.off & dpkt.ip.IP_OFFMASK
-            print('IP: %s -> %s   (len=%d ttl=%d DF=%d MF=%d offset=%d)\n' % \
-              (inet_to_str(ip.src), inet_to_str(ip.dst), ip.len, ip.ttl, do_not_fragment, more_fragments, fragment_offset))
-            if tcp.flags & dpkt.tcp.TH_SYN:
-                print("SYN packet")
-            elif tcp.flags & dpkt.tcp.TH_ACK:
-                print("ACK packet")
-                print(f"Sequence number: {tcp.seq}")
-                print(f"Acknowledgement number: {tcp.ack}")
-                print(f"Receive Window size: {tcp.win}")
-            else:
-                print("Not a SYN or ACK packet")
-            
-            if tcp.flags & dpkt.tcp.TH_SYN:
-                total_bytes = 0
-                start_time = ts
-            elif tcp.flags & dpkt.tcp.TH_ACK:
-                total_bytes += len(tcp.data)
-                elapsed_time = ts - start_time
-                if elapsed_time > 0:
-                    throughput = total_bytes / elapsed_time
-                    print(f"Throughput:", throughput, "bps")
-                    print()
+            key = (srcIp, dstIp, srcPort, dstPort)
+            if key in flows:
+                if flows[key]['first_pkt']:
+                    flows[key]['ack'] = tcp.ack
+                    flows[key]['win_size'] = tcp.win
+                    flows[key]['first_pkt'] = False
+                    flows[key]['seq_nums'] = [tcp.seq]
+                    flows[key]['ack_nums'] = [tcp.ack] 
+                    flows[key]['window_sizes'] = [tcp.win]
                 else:
-                    print("Throughput: N/A\n")
+                    flows[key]['bytes'] += len(tcp.data)
+                    flows[key]['last_seen'] = ts
+                    flows[key]['seq_nums'].append(tcp.seq)
+                    flows[key]['ack_nums'].append(tcp.ack) 
+                    flows[key]['window_sizes'].append(tcp.win)
+            else:
+                flows[key] = {'bytes': len(tcp.data), 'start_time': ts, 'last_seen': ts, 'first_pkt': True, 'ack': 0, 'win_size': 0, 'seq_nums': [], 'ack_nums': [], 'window_sizes': []}
+                flows[key]['seq_nums'].append(tcp.seq) 
+                flows[key]['ack_nums'].append(tcp.ack)
+                flows[key]['window_sizes'].append(tcp.win)
+
+i = 1
+print("TCP FLOW: \n --------------------------------------------------------------")
+for key in flows:
+    if i >= 4:
+        break
+    duration = (flows[key]['last_seen'] - flows[key]['start_time']) / 1000000.0
+    throughput = flows[key]['bytes'] / duration
+    if throughput == 0:
+        continue;
+    print(f"Flow {i}: ")
+    print(f"Source Port: {key[2]}\tSource IP Address: {key[0]}")
+    print(f"Destination Port: {key[3]} \tDestination IP Address: {key[1]}")
+    print(f"Throughput: {throughput:.2f}, bytes/s ")
+    i += 1
+
+i = 1
+
+for key in flows: 
+    if i >= 4:
+        break
+    duration = (flows[key]['last_seen'] - flows[key]['start_time']) / 1000000.0
+    throughput = flows[key]['bytes'] / duration
+    if throughput == 0:
+        continue;
+    print(f"\nFirst two transaction after TCP connection for flow {i}  \n --------------------------------------------------------------")
+    for j in range(2):
+        print(f"Transaction {j+1}:")
+        print(f"Seq number: {flows[key]['seq_nums'][j]}")
+        print(f"Ack number: {flows[key]['ack_nums'][j]}")
+        print(f"Window size: {flows[key]['window_sizes'][j]}")
+    i += 1
 
 f.close()
--- HW 2/fake3.py
+++ HW 2/fake3.py
@@ -22,13 +22,27 @@ for ts, bf in pcap:
             dstIp = socket.inet_ntoa(ip.dst)
             key = (srcIp, dstIp, srcPort, dstPort)
             if key in flows:
-                flows[key]['bytes'] += len(tcp.data)
-                flows[key]['last_seen'] = ts
+                if flows[key]['first_pkt']:
+                    flows[key]['ack'] = tcp.ack
+                    flows[key]['win_size'] = tcp.win
+                    flows[key]['first_pkt'] = False
+                    flows[key]['seq_nums'] = [tcp.seq]
+                    flows[key]['ack_nums'] = [tcp.ack] 
+                    flows[key]['window_sizes'] = [tcp.win]
+                else:
+                    flows[key]['bytes'] += len(tcp.data)
+                    flows[key]['last_seen'] = ts
+                    flows[key]['seq_nums'].append(tcp.seq)
+                    flows[key]['ack_nums'].append(tcp.ack) 
+                    flows[key]['window_sizes'].append(tcp.win)
             else:
-                flows[key] = {'bytes': len(tcp.data), 'start_time': ts, 'last_seen': ts}
-
+                flows[key] = {'bytes': len(tcp.data), 'start_time': ts, 'last_seen': ts, 'first_pkt': True, 'ack': 0, 'win_size': 0, 'seq_nums': [], 'ack_nums': [], 'window_sizes': []}
+                flows[key]['seq_nums'].append(tcp.seq) 
+                flows[key]['ack_nums'].append(tcp.ack)
+                flows[key]['window_sizes'].append(tcp.win)
 
 i = 1
+print("TCP FLOW: \n --------------------------------------------------------------")
 for key in flows:
     if i >= 4:
         break
@@ -40,6 +54,23 @@ for key in flows:
     print(f"Source Port: {key[2]}\tSource IP Address: {key[0]}")
     print(f"Destination Port: {key[3]} \tDestination IP Address: {key[1]}")
     print(f"Throughput: {throughput:.2f}, bytes/s ")
+    i += 1
+
+i = 1
+
+for key in flows: 
+    if i >= 4:
+        break
+    duration = (flows[key]['last_seen'] - flows[key]['start_time']) / 1000000.0
+    throughput = flows[key]['bytes'] / duration
+    if throughput == 0:
+        continue;
+    print(f"\nFirst two transaction after TCP connection for flow {i}  \n --------------------------------------------------------------")
+    for j in range(2):
+        print(f"Transaction {j+1}:")
+        print(f"Seq number: {flows[key]['seq_nums'][j]}")
+        print(f"Ack number: {flows[key]['ack_nums'][j]}")
+        print(f"Window size: {flows[key]['window_sizes'][j]}")
     i += 1
 
 f.close()
