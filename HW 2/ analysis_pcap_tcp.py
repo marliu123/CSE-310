98be560154e447b55246255aba5306081e6c7c88
Marcus Liu <marcus.liu@stonybrook.edu>
2023-03-09 19:10:02 -0500

    edit

--- HW 2/ analysis_pcap_tcp.py
+++ HW 2/ analysis_pcap_tcp.py
@@ -1,5 +1,4 @@
 import dpkt
-import datetime
 import socket
 
 f = open('assignment2.pcap', 'rb')
@@ -8,69 +7,159 @@ pcap = dpkt.pcap.Reader(f)
 def convMac(address):
     return ":".join("{:02x}".format(b) for b in address)
 
+receiverPort = 80
 flows = {}
+time={}
+congest={}
 
+print("PART A")
 for ts, bf in pcap:
     eth = dpkt.ethernet.Ethernet(bf)
-    if isinstance(eth.data, dpkt.ip.IP):
-        ip = eth.data
-        if isinstance(ip.data, dpkt.tcp.TCP):
-            tcp = ip.data
-            srcPort = tcp.sport
-            dstPort = tcp.dport
-            srcIp = socket.inet_ntoa(ip.src)
-            dstIp = socket.inet_ntoa(ip.dst)
-            key = (srcIp, dstIp, srcPort, dstPort)
-            if key in flows:
-                if flows[key]['first_pkt']:
-                    flows[key]['ack'] = tcp.ack
-                    flows[key]['win_size'] = tcp.win
-                    flows[key]['first_pkt'] = False
-                    flows[key]['seq_nums'] = [tcp.seq]
-                    flows[key]['ack_nums'] = [tcp.ack] 
-                    flows[key]['window_sizes'] = [tcp.win]
-                else:
-                    flows[key]['bytes'] += len(tcp.data)
-                    flows[key]['last_seen'] = ts
-                    flows[key]['seq_nums'].append(tcp.seq)
-                    flows[key]['ack_nums'].append(tcp.ack) 
-                    flows[key]['window_sizes'].append(tcp.win)
+    if isinstance(eth.data, dpkt.ip.IP) and isinstance(eth.data.data, dpkt.tcp.TCP):
+        tcp = eth.data.data
+        srcPort = tcp.sport
+        dstPort = tcp.dport
+        srcIp = socket.inet_ntoa(eth.data.src)
+        dstIp = socket.inet_ntoa(eth.data.dst)
+        key = (srcIp, dstIp, srcPort, dstPort)
+        if key in flows:
+            if flows[key]['firstPkt']:
+                flows[key]['ack'] = tcp.ack
+                flows[key]['winSize'] = tcp.win
+                flows[key]['cwnd'] = 1
+                flows[key]['firstPkt'] = False
+                flows[key]['seqNums'] = [tcp.seq]
+                flows[key]['ackNums'] = [tcp.ack] 
+                flows[key]['windowSizes'] = [tcp.win]
             else:
-                flows[key] = {'bytes': len(tcp.data), 'start_time': ts, 'last_seen': ts, 'first_pkt': True, 'ack': 0, 'win_size': 0, 'seq_nums': [], 'ack_nums': [], 'window_sizes': []}
-                flows[key]['seq_nums'].append(tcp.seq) 
-                flows[key]['ack_nums'].append(tcp.ack)
-                flows[key]['window_sizes'].append(tcp.win)
+                flows[key]['bytes'] += len(tcp.data)
+                flows[key]['lastSeen'] = ts
+                flows[key]['seqNums'].append(tcp.seq)
+                flows[key]['ackNums'].append(tcp.ack) 
+                flows[key]['windowSizes'].append(tcp.win)
+                flows[key]['ack'] = tcp.ack
+                flows[key]['winSize'] = tcp.win
+        else:
+            flows[key] = {'bytes': len(tcp.data), 'startTime': ts, 'lastSeen': ts, 'firstPkt': True, 'ack': 0, 'winSize': 0, 'seqNums': [], 'ackNums': [], 'windowSizes': []}
+            flows[key]['seqNums'].append(tcp.seq) 
+            flows[key]['ackNums'].append(tcp.ack)
+            flows[key]['windowSizes'].append(tcp.win)
+        if tcp.flags == 2:
+            time[tcp.sport] = []
+            congest[tcp.sport] = []
+            time[tcp.sport].append(ts)
+        elif tcp.flags == 17 and tcp.sport == receiverPort:
+            time[tcp.dport].append(ts)
+        elif tcp.flags == 24 and tcp.sport != receiverPort:
+            if len(congest[tcp.sport]) < 2:
+                congest[tcp.sport].append(ts)
+                congest[tcp.sport].append(False)
+        elif tcp.flags == 16 and tcp.sport == receiverPort:
+            if not congest[tcp.dport][1]:
+                congest[tcp.dport][0] = ts - congest[tcp.dport][0]
+                congest[tcp.dport][1] = True
+                congest[tcp.dport].append(0)
+    
 
-i = 1
+f.close()
+
+count = 0
+f2 = open('assignment2.pcap','rb')
+pcap = dpkt.pcap.Reader(f2)
+for ts, buf in pcap:
+    eth = dpkt.ethernet.Ethernet(buf)
+    ip = eth.data
+    tcp = ip.data
+    if tcp.flags!=2 and tcp.sport != receiverPort:
+        if len(tcp)>0:
+            if(len(congest[tcp.sport])<3):
+                continue
+            elif (congest[tcp.sport][2]==0 and len(congest[tcp.sport])<6 and len(tcp.data)>0):
+                congest[tcp.sport][2]=ts
+                congest[tcp.sport].append(1)
+            elif (congest[tcp.sport][2]<=ts and ts <= congest[tcp.sport][2]+congest[tcp.sport][0]) and len(tcp.data)>0:
+                congest[tcp.sport][len(congest[tcp.sport])-1]+=1
+            elif (congest[tcp.sport][2]>ts or ts > congest[tcp.sport][2]+congest[tcp.sport][0]):
+                congest[tcp.sport][2]=0
+    count+=1
+
+
+
 print("TCP FLOW: \n --------------------------------------------------------------")
+j = 1
 for key in flows:
-    if i >= 4:
+    if j >= 4:
         break
-    duration = (flows[key]['last_seen'] - flows[key]['start_time']) / 1000000.0
+    duration = (flows[key]['lastSeen'] - flows[key]['startTime']) / 1.0
     throughput = flows[key]['bytes'] / duration
     if throughput == 0:
         continue;
-    print(f"Flow {i}: ")
+    print(f"Flow {j}: ")
     print(f"Source Port: {key[2]}\tSource IP Address: {key[0]}")
     print(f"Destination Port: {key[3]} \tDestination IP Address: {key[1]}")
     print(f"Throughput: {throughput:.2f}, bytes/s ")
-    i += 1
+    j += 1
 
-i = 1
-
+k = 1
 for key in flows: 
-    if i >= 4:
+    if k >= 4:
         break
-    duration = (flows[key]['last_seen'] - flows[key]['start_time']) / 1000000.0
+    duration = (flows[key]['lastSeen'] - flows[key]['startTime']) / 1000000.0
     throughput = flows[key]['bytes'] / duration
     if throughput == 0:
         continue;
-    print(f"\nFirst two transaction after TCP connection for flow {i}  \n --------------------------------------------------------------")
+    print(f"\nFirst two transaction after TCP connection for flow {k}  \n --------------------------------------------------------------")
     for j in range(2):
         print(f"Transaction {j+1}:")
-        print(f"Seq number: {flows[key]['seq_nums'][j]}")
-        print(f"Ack number: {flows[key]['ack_nums'][j]}")
-        print(f"Window size: {flows[key]['window_sizes'][j]}")
-    i += 1
+        print(f"Sender Seq number: {flows[key]['seqNums'][j]}")
+        print(f"Ack number: {flows[key]['ackNums'][j]}")
+        print(f"Window size: {flows[key]['windowSizes'][j]}")
+        print(f"Receiver Seq number: {flows[key]['ackNums'][j]}")
+        print(f"Ack number: {flows[key]['seqNums'][j]}")
+        print(f"Window size: {flows[key]['windowSizes'][j]}\n")
+    k += 1
 
-f.close()
+
+print("PART B")
+print("First 3 congestion windows of each flow")
+m = 1
+for val in congest:
+    print("port:", val)
+    print(f"Window size {m}: {congest[val][3]}, {congest[val][4]}, {congest[val][5]}")
+    m += 1 
+
+f2.close()
+
+duplicates={}
+
+f3 = open('assignment2.pcap','rb')
+pcap3 = dpkt.pcap.Reader(f3)
+
+for ts, buf in pcap3:
+    eth = dpkt.ethernet.Ethernet(buf)
+    ip = eth.data
+    tcp = ip.data
+    if tcp.flags==2:
+        duplicates[tcp.sport]={}
+    elif tcp.flags==16 and tcp.sport == receiverPort:
+        duplicate = list(duplicates[tcp.dport].keys())
+        if not tcp.ack in duplicate:
+            duplicates[tcp.dport][tcp.ack]=0
+        else:
+            duplicates[tcp.dport][tcp.ack]+=1
+
+print("\nTriple Duplicate ACKS and Timeout")
+n = 1
+for val in duplicates:
+    timeout = 0
+    triple = 0
+    for val2 in duplicates[val]:
+        if (duplicates[val][val2]>0):
+            timeout += 1
+        if(duplicates[val][val2]>84):
+            triple += 1
+    print("port:", val)
+    print("Triple Duplicate ACK: ",triple)
+    print("Timeout: ",timeout)
+
+f3.close()
